7af2570310a168f37b78723dcd7d9358abb8f6d4
saidPREDATOR <112885798+sv9tik1@users.noreply.github.com>
2023-12-07 21:44:44 +0300

    Add files via upload

---
+++ che/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+    var a = new int[10, 10];
+    for (int x = 0; x < a.GetLength(0); x++)
+    {
+        int i = 0;
+        for (int y = 0; y < a.GetLength(0); y++)
+        {
+            if (y >= x)
+                a[x, y] = i++;
+        }
+    }
+    Console.Write('\n');
+    for (int x = 0; x < a.GetLength(0); x++)
+    {
+        for (int y = 0; y < a.GetLength(0); y++)
+        {
+            Console.Write(a[x, y]);
+        }
+        Console.Write('\n');
+    }
+}
+    }
